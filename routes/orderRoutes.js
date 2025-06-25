ab7020aaa23a3a7723f6324fdf35eb6338e06faf
Awais Ahmad Khan <114736094+theawaisahmadkhan@users.noreply.github.com>
2025-06-25 22:25:11 +0500

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/orderController');
 
+
 router.get('/', ctrl.listOrders);
 router.get('/:id', ctrl.getOrder);
 router.post('/', ctrl.createOrder);
